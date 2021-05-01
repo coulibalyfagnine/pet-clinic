6bd562e8f2c505108152be07abf98dd6a67e7d58
Fagnine <coulibalyfagnine@gmail.com>
2021-05-01 16:03:16 +0200

    updated PetClinicApplication main method to print 'pet clinc' in console when application started

--- src/main/java/com/alas/petclinic/PetClinicApplication.java
+++ src/main/java/com/alas/petclinic/PetClinicApplication.java
@@ -2,12 +2,16 @@ package com.alas.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class PetClinicApplication {
+public class PetClinicApplication  {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
+		ApplicationContext ctx =  SpringApplication.run(PetClinicApplication.class, args);
+		System.out.println("pet clinic");
+		System.out.println(ctx);
+		
 	}
 
 }
