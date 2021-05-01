7f00afda86e0eaa66e94e252552aae9543fcdd64
Fagnine <coulibalyfagnine@gmail.com>
2021-05-01 18:52:00 +0200

    add POJO (pet, owner, vet, petType and person) to model package

--- src/main/java/com/alas/petclinic/PetClinicApplication.java
+++ src/main/java/com/alas/petclinic/PetClinicApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.alas.petclinic.model.Person;
+
 @SpringBootApplication
 public class PetClinicApplication  {
 
@@ -11,6 +13,9 @@ public class PetClinicApplication  {
 		ApplicationContext ctx =  SpringApplication.run(PetClinicApplication.class, args);
 		System.out.println("pet clinic");
 		System.out.println(ctx);
+		
+		Person paul = new Person();
+		System.out.println(paul.getFirstName());
 		
 	}
 
